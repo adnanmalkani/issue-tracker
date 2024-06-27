aa304dddb78d9b05683fffabd9a7503c6a36b425
Adnan Malkani <adnanmalkani@hotmail.com>
2024-06-27 03:39:25 +0100

    Update Issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -32,7 +32,9 @@ const IssueForm = ({ issue }: { issue?: issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post('/api/issues', data), router.push('/issues');
+      if (issue) await axios.patch('/api/issues/' + issue.id, data);
+      else await axios.post('/api/issues', data);
+      router.push('/issues');
     } catch (error) {
       setSubmitting(false);
       setError('An unexpected error occurred.');
@@ -67,7 +69,8 @@ const IssueForm = ({ issue }: { issue?: issue }) => {
           <ErrorMessage>{errors.description.message}</ErrorMessage>
         )}
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
+          {issue ? 'Update Issue' : 'Submit New Issue'}{' '}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
